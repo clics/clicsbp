6a002d0eddf7132130f6a3c600703546e8530874
lingulist <mattis.list@lingpy.org>
2022-10-24 07:06:16 +0200

    update scores

--- clicsbpcommands/piecharts.py
+++ clicsbpcommands/piecharts.py
@@ -30,6 +30,11 @@ def run(args):
             delimiter="\t",
             dicts=True)
     rels = {}
+    props = {
+            "Adjacency": 0,
+            "Shape": 0,
+            "Function": 0
+            }
     for row in relations:
         # change choice() to row["Relation"] !
         if row["NodeA"] != row["NodeB"]:
@@ -38,8 +43,15 @@ def run(args):
                     int(row["Shape"]),
                     int(row["Function"])
                     )
+            props["Adjacency"] += int(row["Adjacency"])
+            props["Shape"] += int(row["Shape"])
+            props["Function"] += int(row["Function"])
             rels[row["NodeB"], row["NodeA"]] = rels[row["NodeA"], row["NodeB"]]
 
+    
+    props["Adjacency"] = props["Adjacency"] / sum(props.values())
+    props["Shape"] = props["Shape"] / sum(props.values())
+    props["Function"] = props["Function"] / sum(props.values())
 
     data = defaultdict(lambda : defaultdict(list))
     for row in colexifications:
@@ -47,7 +59,7 @@ def run(args):
                 (
                             row["CONCEPT"], 
                             row["COMMUNITY"],
-                            row["LINKS"].split(";")
+                            row["COLEXIFICATIONS"].split(";")
                             )
                         ]
     colors = [
@@ -55,6 +67,7 @@ def run(args):
         "#1f78b4",
         "#b2df8a"]
     pies = {}
+    graphs = {}
     for fam in data:
         plt.clf()
         args.log.info("loading {0}".format(fam))
@@ -81,9 +94,19 @@ def run(args):
                 this_pie[2] += rels[nodeA, nodeB][2]
             except:
                 print(fam, nodeA, nodeB)
-        pies[fam] = this_pie
-        plt.pie(this_pie, colors=colors, labels=["Adjacency", "Shape", "Function"])
-        plt.title("Family {0} / {1} edges".format(fam, len(G.edges)))
+        prop_pie = [
+                this_pie[0] * props["Adjacency"],
+                this_pie[1] * props["Shape"],
+                this_pie[2] * props["Function"]
+                ]
+
+        pies[fam] = (this_pie, prop_pie, len(G), len(G.edges))
+        fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+        ax1.pie(this_pie, colors=colors, labels=["Adjacency", "Shape", "Function"])
+        ax1.set_title("Raw Scores")
+        ax2.pie(prop_pie, colors=colors, labels=["Adjacency", "Shape", "Function"])
+        ax2.set_title("Weighted by Proportions")
+        fig.suptitle("Family {0} / {1} edges".format(fam, len(G.edges)))
         plt.savefig(
                 clicsbp.dir.joinpath(
                     "output", 
@@ -94,15 +117,18 @@ def run(args):
     with open(clicsbp.dir.joinpath(
         "output",
         "pie-chart-data.tsv"), "w") as f:
-        for fam, pie in pies.items():
-            f.write("{0}\t{1}\t{2}\t{3}\t{4:.2f}\t{5:.2f}\t{6:.2f}\n".format(
+        for fam, (pie, pieprop, leng, lengedges) in pies.items():
+            f.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6:.2f}\t{7:.2f}\t{8:.2f}\n".format(
                 fam,
+                str(leng),
+                str(lengedges),
                 pie[0],
                 pie[1],
                 pie[2],
-                pie[0]/len(rels),
-                pie[1]/len(rels),
-                pie[2]/len(rels)))
+                pieprop[0],
+                pieprop[1],
+                pieprop[2])
+                )
 
 
 
